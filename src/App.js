df2d88b579fc6b3511221db856358ecaa8b86aea
Rinku Kumar Maurya <rinkukumarmaurya221@gmail.com>
2021-12-13 18:55:55 +0530

    integrate Alan AI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import alanBtn from '@alan-ai/alan-sdk-web'
+
+const alanKey = process.env.REACT_APP_ALAN_KEY;
+
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command }) => {
+                if(command === 'testing'){
+                    alert('Alan is working...');
+                }
+            },
+        })
+    }, [])
+
+    return (
+        <div>
+            <h1>ALAN AI</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
