01734664f38a073687423c231e272286f97700fa
Rinku Kumar Maurya <rinkukumarmaurya221@gmail.com>
2021-12-14 12:38:31 +0530

    redesigned landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
-
-import NewsCards from './components/NewsCards/NewsCards'
-
 import alanBtn from '@alan-ai/alan-sdk-web'
 
+import NewsCards from './components/NewsCards/NewsCards'
+import useStyles from './styles.js'
+
 const alanKey = process.env.REACT_APP_ALAN_KEY;
+const alanLogoSrc = 'https://alan.app/static/meduza_logo.3d2c10da.png';
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -22,7 +24,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>ALAN AI</h1>
+            <div className={classes.logoContainer}>
+                <img className={classes.alanLogo} src={alanLogoSrc} alt='Alan Logo'/>
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     )
